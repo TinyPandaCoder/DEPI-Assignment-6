441392cf095695c98ebbf70257941e3374bb353a
Mahmoud Alaa <asticalaa1@gmail.com>
2024-08-20 06:59:24 +0300

    Everything is perfect main and nav bar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // Navigation Bar
 let lastScrollTop = 0;
 const navbar = document.querySelector("#navbar");
+
 window.addEventListener("scroll", function () {
   const navbarHeight = -navbar.offsetHeight + 3;
-  if (window.scrollY === 0) {
-    navbar.classList.add("bg-transparent");
+  if (window.scrollY != 0) {
+    navbar.style.background = "#333333";
   } else {
-    navbar.classList.remove("bg-transparent");
+    navbar.classList.add("bg-transparent");
   }
   if (window.scrollY - lastScrollTop > 0) {
     navbar.style.top = navbarHeight.toString() + "px";
@@ -23,7 +24,6 @@ window.addEventListener("scroll", function () {
 window.addEventListener("resize", () => {
   const windowWidth = window.innerWidth;
   const navbarNav = document.querySelector("#navbarNav");
-  console.log(windowWidth);
   if (windowWidth >= 768) {
     const menuButton = document.getElementById("menu-button");
     menuButton.setAttribute("aria-expanded", "false");
@@ -31,3 +31,30 @@ window.addEventListener("resize", () => {
   }
 });
 // Zoom Closes menu
+
+// // Padding to main section equal to nav bar
+// const mainSection = document.querySelector("#main");
+// mainSection.style.padding = `${navbar.offsetHeight}px 0px 0px 0px`;
+// // Padding to main section equal to nav bar
+
+// main section
+const professions = document.querySelectorAll("#word");
+let i = 100,
+  pos = 0;
+let speed = 0.3;
+let sign = -1;
+const professionsContainer = document.getElementById("next-word");
+setInterval(() => {
+  i += speed * sign;
+  professionsContainer.style.width = i + "%";
+  if (i <= 0 || i >= 100) {
+    sign *= -1;
+  }
+  if (i <= 0) {
+    professions[pos].classList.add("d-none");
+    pos = (pos + 1) % professions.length;
+    professions[pos].classList.remove("d-none");
+    console.log(pos);
+  }
+}, 1);
+// main section
